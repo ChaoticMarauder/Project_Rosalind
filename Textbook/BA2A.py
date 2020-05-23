a28c8f82707809c82df104fb50b13ab5c7b77ebb
Soham Dibyachintan <sdibyachintan1@gmail.com>
2020-05-23 18:58:57 +0530

    Added Textbook BA1B

--- Textbook/BA2A.py
+++ Textbook/BA2A.py
@@ -1,10 +1,4 @@
-def PatternCount(dna, pattern):
-    count = 0
-    len_pattern = len(pattern) 
-    for i in range(len(dna)-len(pattern)):
-        if(dna[i:i+len_pattern] == pattern):
-            count+=1
-    return count
+from Chapter1 import PatternCount
 
 def frequent_words(dna, k):
     dict_kmer={}
@@ -27,3 +21,17 @@ def frequent_words(dna, k):
             list_frequent_kmers.append(key)
             
     return list_frequent_kmers
+
+def main():
+    with open('datasets/rosalind_ba1b.txt') as input_file:
+        dna, k = input_file.read().strip().split('\n')
+        
+    list_frequent_kmers = frequent_words(dna, int(k))
+    
+    print(' '.join(list_frequent_kmers))
+    
+    with open('solutions/rosalind_ba1b.txt', 'w') as output_file:
+        output_file.write(' '.join(list_frequent_kmers))
+        
+if(__name__=='__main__'):
+    main()
