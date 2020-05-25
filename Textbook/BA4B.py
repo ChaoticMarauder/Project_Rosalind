008691ec9c9ecc934175c2f222131decf6385817
Soham Dibyachintan <sdibyachintan1@gmail.com>
2020-05-25 21:07:37 +0530

    Added Textbook BA4B

--- Textbook/BA4B.py
+++ Textbook/BA4B.py
@@ -1,45 +1,5 @@
 from Chapter1 import reverse_complement
 
-def translate(rna):
-    dic = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
-       "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
-       "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
-       "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
-       "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
-       "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
-       "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
-       "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
-       "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
-       "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
-       "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
-       "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
-       "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
-       "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
-       "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
-       "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
-    protein=""
-    l=len(rna)
-    start_codon=0
-    end_codon=1
-    start=rna.find("AUG")
-    if start!=-1:
-        start_codon=start
-        while start+2<l:
-            i=rna[start:start+3]
-            if (i=="UAA" or i=="UAG" or i=="UGA"):
-                end_codon=start
-                break
-            for j in dic:
-                if(j==i):
-                    protein=protein+dic[i]
-            start=start+3
-    
-    if(end_codon>1):
-        return protein, start_codon, end_codon
-    else:
-        return "",start_codon, end_codon
-    
-    
 def transcribe(dna):
     rna=[]
     for base in dna:
@@ -103,3 +63,21 @@ def encode_peptide(dna, peptide):
                 list_dna.append(reverse_complement(pattern))       
 
     return list_dna
+
+
+def main():
+    with open('datasets/rosalind_ba4b.txt') as input_file:
+        dna, peptide = input_file.read().strip().split('\n')
+        
+    list_dna = encode_peptide(dna, peptide)
+    
+    print('\n'.join(list_dna))
+    
+    with open('solutions/rosalind_ba4b.txt', 'w') as output_file:
+        output_file.write('\n'.join(list_dna))
+        
+if(__name__=='__main__'):
+    main()
+        
+    
+    
